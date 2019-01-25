637050ec4018e47dfb204573a0db0d8d39b08daf
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2019-01-25 21:31:09 +0530

    crated blog

--- first/settings.py
+++ first/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
-    'album.apps.AlbumConfig'
+    'album.apps.AlbumConfig',
+    'blog',
 
 ]
 
